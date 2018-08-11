3a830b3893d0cc66974786e305353d72a37a22af
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 02:40:33 -0700

    crdroid: Smart Pixels [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I4f8c1eceed1f0ac98d3f81737077edd88422c479

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -43,6 +43,7 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.fragments.ui.DozeSettings;
 import com.crdroid.settings.fragments.ui.PulseSettings;
+import com.crdroid.settings.fragments.ui.SmartPixels;
 import com.crdroid.settings.utils.DeviceUtils;
 
 import java.util.ArrayList;
@@ -51,16 +52,32 @@ import java.util.List;
 @SearchIndexable
 public class UserInterface extends SettingsPreferenceFragment {
 
+    public static final String TAG = "UserInterface";
+
+    private static final String SMART_PIXELS = "smart_pixels";
+
+    private Preference mSmartPixels;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.crdroid_settings_ui);
+
+        Context mContext = getActivity().getApplicationContext();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
+        boolean mSmartPixelsSupported = getResources().getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported)
+            prefScreen.removePreference(mSmartPixels);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         DozeSettings.reset(mContext);
         PulseSettings.reset(mContext);
+        SmartPixels.reset(mContext);
         Settings.System.putIntForUser(resolver,
                 Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT);
     }
@@ -80,6 +97,10 @@ public class UserInterface extends SettingsPreferenceFragment {
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
 
+                    boolean mSmartPixelsSupported = context.getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportSmartPixels);
+                    if (!mSmartPixelsSupported)
+                        keys.add(SMART_PIXELS);
                     return keys;
                 }
             };
