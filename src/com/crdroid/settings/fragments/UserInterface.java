4bf66926ab46af570df81019be304cfd8123893d
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 02:40:33 -0700

    crdroid: Smart Pixels [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I4f8c1eceed1f0ac98d3f81737077edd88422c479

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -42,6 +42,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.fragments.ui.DozeSettings;
+import com.crdroid.settings.fragments.ui.SmartPixels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,11 +63,18 @@ public class UserInterface extends SettingsPreferenceFragment {
 
         Context mContext = getActivity().getApplicationContext();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
+        boolean mSmartPixelsSupported = getResources().getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported)
+            prefScreen.removePreference(mSmartPixels);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         DozeSettings.reset(mContext);
+        SmartPixels.reset(mContext);
     }
 
     @Override
@@ -83,6 +91,11 @@ public class UserInterface extends SettingsPreferenceFragment {
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
+
+                    boolean mSmartPixelsSupported = context.getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportSmartPixels);
+                    if (!mSmartPixelsSupported)
+                        keys.add(SMART_PIXELS);
                     return keys;
                 }
             };
