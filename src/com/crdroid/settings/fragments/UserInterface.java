06ca4e5008980c35df65d59208435f78da01434a
Pranav Vashi <neobuddy89@gmail.com>
2019-12-16 00:16:19 +0530

    Allow tuning ambient display with sensors [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I8269505ffc243c521cb4f9b91fd923441fe6a43b

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -41,6 +41,8 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.crdroid.settings.fragments.ui.DozeSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -103,6 +105,7 @@ public class UserInterface extends SettingsPreferenceFragment implements
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
+        DozeSettings.reset(mContext);
     }
 
     @Override
