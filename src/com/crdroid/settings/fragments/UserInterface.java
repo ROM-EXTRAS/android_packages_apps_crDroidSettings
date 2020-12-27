c03de061ac7e43edcdc5ec2f43b0060de2e0a135
ezio84 <brabus84@gmail.com>
2020-12-27 17:29:21 +0530

    Pulse initial checkin for Android 13 [2/2]
    * Pulse: Solid renderer round lines [2/2]
    
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    
    Change-Id: Ifbb50c4e6c75eb06657d5c626379a53c8efaffbe

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -42,6 +42,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.fragments.ui.DozeSettings;
+import com.crdroid.settings.fragments.ui.PulseSettings;
 import com.crdroid.settings.fragments.ui.SmartPixels;
 
 import java.util.ArrayList;
@@ -107,6 +108,7 @@ public class UserInterface extends SettingsPreferenceFragment implements
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         DozeSettings.reset(mContext);
+        PulseSettings.reset(mContext);
         SmartPixels.reset(mContext);
     }
 
