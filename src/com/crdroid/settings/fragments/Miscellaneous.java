b5f0656722b3dcebed1570c599f2e2c2b1fe28fa
spezi77 <spezi7713@gmx.net>
2022-01-31 01:01:52 +0100

    Misc: User toggle for unlimited photos storage [2/2]
    Change-Id: Ic42dee1857873004f184ca008239a312f3c9cc51

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -47,15 +47,15 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     public static final String TAG = "Miscellaneous";
 
     // private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-    // private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
     // private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
 
     // private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    // private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     // private static final String SYS_NETFLIX_SPOOF = "persist.sys.spoof_netflix";
 
     // private SwitchPreference mGamesSpoof;
-    // private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mPhotosSpoof;
     // private SwitchPreference mNetFlixSpoof;
 
     @Override
@@ -71,9 +71,9 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         // mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
         // mGamesSpoof.setOnPreferenceChangeListener(this);
 
-        // mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
-        // mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        // mPhotosSpoof.setOnPreferenceChangeListener(this);
+        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
 
         // mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
         // mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
@@ -82,14 +82,15 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // if (preference == mGamesSpoof) {
+        if (preference == mPhotosSpoof) {
+                boolean value = (Boolean) newValue;
+                SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
+                SystemPropPoker.getInstance().poke();
+                return true;
+        }
+        // else if (preference == mGamesSpoof) {
         //     boolean value = (Boolean) newValue;
         //     SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-        //     SystemPropPoker.getInstance().poke();
-        //     return true;
-        // } else if (preference == mPhotosSpoof) {
-        //     boolean value = (Boolean) newValue;
-        //     SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
         //     SystemPropPoker.getInstance().poke();
         //     return true;
         // } else if (preference == mNetFlixSpoof) {
@@ -103,6 +104,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
+        SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
     }
 
     @Override
