85c6758e7c357ae64e372c252ecafbc7d6408073
Joey Huab <joey@evolution-x.org>
2022-02-13 14:52:44 +0900

    Misc: PixelPropUtils: User toggle for StreamProp [2/2]
    Change-Id: I5ea8d4b456c4c4d8b1f5fb0a9c550c10ced001f2

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -45,12 +45,15 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_STREAM_SPOOF = "use_stream_spoof";
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_STREAM_SPOOF = "persist.sys.pixelprops.streaming";
 
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mStreamSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,6 +71,10 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        mStreamSpoof = (SwitchPreference) findPreference(KEY_STREAM_SPOOF);
+        mStreamSpoof.setChecked(SystemProperties.getBoolean(SYS_STREAM_SPOOF, true));
+        mStreamSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -80,6 +87,10 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
+        } else if (preference == mStreamSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_STREAM_SPOOF, value ? "true" : "false");
+            return true;
         }
         return false;
     }
@@ -89,6 +100,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
         SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
+        SystemProperties.set(SYS_STREAM_SPOOF, "true");
     }
 
     @Override
