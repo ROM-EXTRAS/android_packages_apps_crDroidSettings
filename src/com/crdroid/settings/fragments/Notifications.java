9962a130a860baafdeada51c39a2dd018bb6bd48
Pranav Vashi <neobuddy89@gmail.com>
2022-04-15 23:48:49 +0530

    HeadsUp: add timeout option (2/2)
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I6f85ef692a02c51197d2badf3ed084e2555a3878

--- src/com/crdroid/settings/fragments/Notifications.java
+++ src/com/crdroid/settings/fragments/Notifications.java
@@ -47,6 +47,10 @@ public class Notifications extends SettingsPreferenceFragment {
 
     public static final String TAG = "Notifications";
 
+    private static final String HEADS_UP_TIMEOUT_PREF = "heads_up_timeout";
+
+    private CustomSeekBarPreference mHeadsUpTimeOut;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +61,22 @@ public class Notifications extends SettingsPreferenceFragment {
         final Context mContext = getActivity().getApplicationContext();
         final ContentResolver resolver = mContext.getContentResolver();
         final Resources res = mContext.getResources();
+
+        mHeadsUpTimeOut = (CustomSeekBarPreference)
+                            prefScreen.findPreference(HEADS_UP_TIMEOUT_PREF);
+        mHeadsUpTimeOut.setDefaultValue(getDefaultDecay(mContext));
+    }
+
+    private static int getDefaultDecay(Context context) {
+        int defaultHeadsUpTimeOut = 5;
+        Resources systemUiResources;
+        try {
+            systemUiResources = context.getPackageManager().getResourcesForApplication("com.android.systemui");
+            defaultHeadsUpTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null)) / 1000;
+        } catch (Exception e) {
+        }
+        return defaultHeadsUpTimeOut;
     }
 
     public static void reset(Context mContext) {
@@ -65,6 +85,8 @@ public class Notifications extends SettingsPreferenceFragment {
                 Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LESS_BORING_HEADS_UP, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.HEADS_UP_TIMEOUT, getDefaultDecay(mContext), UserHandle.USER_CURRENT);
     }
 
     @Override
