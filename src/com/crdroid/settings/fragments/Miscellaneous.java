cab3593308675f19a19746b5cde58bcf16a6312b
droidfreak32 <rushabshah32@gmail.com>
2025-01-02 03:14:37 +0530

    fixup! ppu
    Change-Id: Ia7c9aa765c3a0d7ce36283af2de4b8925cc6a97d

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -46,17 +46,17 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     public static final String TAG = "Miscellaneous";
 
-    // private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    // private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
+    private static final String KEY_GAMES_SPOOF = "persist.sys.gamehooks.enable";
+    private static final String KEY_PHOTOS_SPOOF = "persist.sys.gphooks.enable";
+    private static final String KEY_PLAYSTORE_SPOOF = "persist.sys.vending.enable";
 
-    // private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    // private static final String SYS_NETFLIX_SPOOF = "persist.sys.spoof_netflix";
+    private static final String SYS_GAMES_SPOOF = "persist.sys.gamehooks.enable";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.gphooks.enable";
+    private static final String SYS_PLAYSTORE_SPOOF = "persist.sys.vending.enable";
 
-    // private SwitchPreference mGamesSpoof;
+    private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
-    // private SwitchPreference mNetFlixSpoof;
+    private SwitchPreference mPlayStoreSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,17 +67,17 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
 
-        // mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
-        // mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
-        // mGamesSpoof.setOnPreferenceChangeListener(this);
+        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
 
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
 
-        // mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
-        // mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
-        // mNetFlixSpoof.setOnPreferenceChangeListener(this);
+        mPlayStoreSpoof = (SwitchPreference) findPreference(KEY_PLAYSTORE_SPOOF);
+        mPlayStoreSpoof.setChecked(SystemProperties.getBoolean(SYS_PLAYSTORE_SPOOF, false));
+        mPlayStoreSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -87,24 +87,25 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
                 SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
                 SystemPropPoker.getInstance().poke();
                 return true;
+        } else if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
+        } else if (preference == mPlayStoreSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_PLAYSTORE_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
         }
-        // else if (preference == mGamesSpoof) {
-        //     boolean value = (Boolean) newValue;
-        //     SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-        //     SystemPropPoker.getInstance().poke();
-        //     return true;
-        // } else if (preference == mNetFlixSpoof) {
-        //     boolean value = (Boolean) newValue;
-        //     SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
-        //     SystemPropPoker.getInstance().poke();
-        //     return true;
-        // }
         return false;
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
+        SystemProperties.set(SYS_GAMES_SPOOF, "false");
+        SystemProperties.set(SYS_PLAYSTORE_SPOOF, "false");
     }
 
     @Override
